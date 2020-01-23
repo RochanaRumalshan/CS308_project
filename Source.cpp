d04832b35f2529f897bc5d107b64a80368798c4c
Rochana <rochanarumalshan@gmail.com>
2020-01-23 12:37:58 +0530

    edited manos code

--- Source.cpp
+++ Source.cpp
@@ -15,17 +15,17 @@ GLfloat specref[] = { 0.8, 0.8, 0.8, 1.0 };
 GLfloat position[] = { 10, 100, 150, 1.0 };
 
 float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle = 0.0;
-float lastx, lasty; 
+float lastx, lasty;
 
 
 void init(void) {
 	glShadeModel(GL_SMOOTH);
-	
+
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
-	
+
 
 	glEnable(GL_COLOR_MATERIAL);
 	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
@@ -34,21 +34,18 @@ void init(void) {
 }
 
 void enable(void) {
-	glEnable(GL_DEPTH_TEST); 
+	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE);
-	glEnable(GL_LIGHTING); 
-	glEnable(GL_LIGHT0); 
-	glShadeModel(GL_SMOOTH); 
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glShadeModel(GL_SMOOTH);
 
 }
 
-
-
-
 void camera(void) {
-	glRotatef(xrot, 1.0, 0.0, 0.0);  
-	glRotatef(yrot, 0.0, 1.0, 0.0); 
-	glTranslated(-xpos, -ypos, -zpos); 
+	glRotatef(xrot, 1.0, 0.0, 0.0);
+	glRotatef(yrot, 0.0, 1.0, 0.0);
+	glTranslated(-xpos, -ypos, -zpos);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 }
 
@@ -64,11 +61,17 @@ void display(void) {
 	glLoadIdentity();
 	camera();
 	enable();
-	
+
 	glColor3f(1.0, 0.0, 0.0);
 	GLUquadricObj* cylin = gluNewQuadric();
 	//gluCylinder(cylin, 0.5, 0.5, 10, 20, 20);
 	//gluDisk(cylin, 0.5, 1.0, 20, 20);
+
+	glPushMatrix();
+	glColor3fv(building1);
+	glutSolidCube(6.0);
+	glTranslated(0, 0, 0);
+	glPopMatrix();
 
 
 	glPushMatrix();
@@ -77,12 +80,7 @@ void display(void) {
 	glutSolidCube(6.0);
 	glPopMatrix();
 
-	glPushMatrix();
-	glColor3fv(building1);
-	glutSolidCube(6.0);
-	glTranslated(0, 0, 0);
-	glPopMatrix();
-
+	
 	char _facName[20];
 	strcpy_s(_facName, "Sarasavi Uyana");
 	glPushMatrix();
@@ -91,7 +89,7 @@ void display(void) {
 	glRotated(90, 0, 1, 0);
 	glRotated(180, 0, 1, 0);
 
-	glTranslated(-1, 3, +1);
+	glTranslated(-1, +3, 1);
 	glScaled(.009, .02, .02);
 	for (int i = 0; i < 15; i++)
 		glutStrokeCharacter(GLUT_STROKE_ROMAN, _facName[i]);
@@ -99,21 +97,21 @@ void display(void) {
 
 
 	glPushMatrix();
-	
+
 	glBegin(GL_QUAD_STRIP);
-	
+
 	glColor3fv(building2);
-	
-	glVertex3d(0,   -4, -20);
+
+	glVertex3d(0, -4, -20);
 	glVertex3d(-10, -4, -20);
 	glVertex3d(0, -1, -20);
 	glVertex3d(-10, -1, -20);
 
 	glVertex3d(0, -1, 20);
 	glVertex3d(-10, -1, 20);
-	glVertex3d(0,   -4, 20);
+	glVertex3d(0, -4, 20);
 	glVertex3d(-10, -4, 20);
-	
+
 	glVertex3d(-10, -4, 20);
 	glVertex3d(-10, -4, -20);
 	glVertex3d(-10, -1, 20);
@@ -137,14 +135,14 @@ void reshape(int w, int h) {
 }
 
 void keyboard(unsigned char key, int x, int y) {
-	if (key =='8')
+	if (key == 'q')
 	{
 		xrot += 1;
 		if (xrot > 360)
 			xrot -= 360;
 	}
-	 
-	if (key == '2')
+
+	if (key == 'z')
 	{
 		xrot -= 1;
 		if (xrot < -360) xrot += 360;
@@ -170,7 +168,7 @@ void keyboard(unsigned char key, int x, int y) {
 		ypos += float(sin(xrotrad));
 	}
 
-	if (key == 'd')
+	if (key == '.')
 	{
 		float yrotrad;
 		yrotrad = (yrot / 180 * PI);
@@ -178,7 +176,7 @@ void keyboard(unsigned char key, int x, int y) {
 		zpos += float(sin(yrotrad)) * 0.2;
 	}
 
-	if (key == 'a')
+	if (key == ',')
 	{
 		float yrotrad;
 		yrotrad = (yrot / 180 * PI);
@@ -186,13 +184,13 @@ void keyboard(unsigned char key, int x, int y) {
 		zpos -= float(sin(yrotrad)) * 0.2;
 	}
 
-	if (key == '6')
+	if (key == 'd')
 	{
 		yrot += 1;
 		if (yrot > 360) yrot -= 360;
 	}
 
-	if (key == '4')
+	if (key == 'a')
 	{
 		yrot -= 1;
 		if (yrot < -360)yrot += 360;
@@ -209,8 +207,8 @@ void mouseMovement(int x, int y) {
 	int diffy = y - lasty;
 	lastx = x;
 	lasty = y;
-	xrot += (float)diffy/10;
-	yrot += (float)diffx/10;
+	xrot += (float)diffy / 20;
+	yrot += (float)diffx / 20;
 
 }
 
@@ -219,7 +217,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("MAP - Wijewardene Hall to Alwis pond");
+	glutCreateWindow("Sarasavi Uyana Railway Station");
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
 	glutReshapeFunc(reshape);
